d8d0b45de325ceb6e212e36804d647280d6364c3
Fauner Echeverri <120021440+Faebb@users.noreply.github.com>
2024-10-29 20:57:17 -0500

    fix(migrations): :fire: fix migration users

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -36,14 +36,14 @@ return new class extends Migration
 
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
-            $table->unsignedBigInteger('id_user')->nullable();
+            $table->unsignedBigInteger('user_id')->nullable();
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
             $table->longText('payload');
             $table->integer('last_activity');
 
             //Foreign Keys
-            $table->foreign('id_user')->references('id_user')->on('users');
+            $table->foreign('user_id')->references('id_user')->on('users');
 
             //Índices
             $table->index('last_activity');
